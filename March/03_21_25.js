b78ba61c363e466482566983f0d73289dacd29f4
Your Name <you@example.com>
2025-03-21 11:11:42 -0700

    completed the challange

---
+++ March/03_21_25.js
@@ -0,0 +1,19 @@
+// Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+
+// For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+
+const numbers = [1, 2, 3, 4, 5, 6, 7];
+const k = 50;
+
+function addNumbersAndK(numbers, k) {
+  for (i = 0; i < numbers.length; i++) {
+    for (j = 0; j < numbers.length; j++) {
+      if (numbers[i] + numbers[j] === k) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
+
+console.log(addNumbersAndK(numbers, k));
